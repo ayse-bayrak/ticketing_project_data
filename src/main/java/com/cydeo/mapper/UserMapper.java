0082243ecdf920d5811bb4c5a8e237705d73573c
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-14 22:51:41 +0300

    UserMapper and UserServiceImpl are created

---
+++ src/main/java/com/cydeo/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package com.cydeo.mapper;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    private final ModelMapper modelMapper;
+
+    public UserMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public UserDTO convertToDTO(User entity) {
+        return modelMapper.map(entity, UserDTO.class);
+    }
+
+    public User convertToEntity(User dto){
+        return modelMapper.map(dto, User.class);
+    }
+}
