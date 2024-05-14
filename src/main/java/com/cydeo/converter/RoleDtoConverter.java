decf98f9f8ca23df9c7bbdd85b04e8d85a7d4cd6
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-14 11:27:20 +0300

    RoleDTOConverter and UserDTOConverter

---
+++ src/main/java/com/cydeo/converter/RoleDtoConverter.java
@@ -0,0 +1,32 @@
+package com.cydeo.converter;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.RoleService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+//when I chose to Role in UI, it is coming as a String But I need tp pass as an object
+//we need convert them
+@Component
+@ConfigurationPropertiesBinding
+public class RoleDtoConverter implements Converter<String, RoleDTO> {
+
+    RoleService roleService;
+
+    public RoleDtoConverter(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @Override
+    public RoleDTO convert(String source) {
+
+        if (source == null || source.equals("")) {  //  Select  -> ""
+            return null;
+        }
+
+        return roleService.findById(Long.parseLong(source));
+
+    }
+
+}
