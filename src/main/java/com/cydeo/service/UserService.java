dc7a9c3f95634133cc42e69905afb68871dd8522
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-14 22:19:49 +0300

    UserService is created

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface UserService {
 
-    List<UserDTO> listAllUsers();
+    List<UserDTO> listAllUsers();//list all
 
     UserDTO findByUserName(String username); //to update--> we need this one to update
 
